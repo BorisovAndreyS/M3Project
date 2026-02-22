84fa8774d12b644a2e7c43a88a043f85cf391db7
helmut <helmut.andrey@gmail.com>
2026-02-22 10:29:53 +0500

    Added userapp full

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from users.forms import UserCreationForm
+
 
 # Create your views here.
+
+class UserCreationView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
