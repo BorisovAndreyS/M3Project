181166460ebffc0e97b8908d511bfcb7d6235582
helmut <helmut.andrey@gmail.com>
2026-02-28 10:44:59 +0500

    products-detail.html productsdetail view work

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.utils.text import slugify
 
 
 class JournalizedModel(models.Model):
@@ -26,12 +27,14 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return f'/products/category/{self.slug}/'
+        return f'/product/category/{self.slug}/'
+
+
 
 
 class Product(JournalizedModel):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True,)
     description = models.TextField(max_length=1000)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -49,10 +52,22 @@ class Product(JournalizedModel):
     def get_absolute_url(self):
         return f'/products/{self.slug}/'
 
+#Создаем автоматический slug
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
 
 class Review(JournalizedModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    rating = models.PositiveIntegerField(validators=[MinLengthValidator(1), MaxLengthValidator(5)])
+    rating = models.PositiveIntegerField(validators=[MinValueValidator (1), MaxValueValidator (5)])
     comment = models.TextField(max_length=1000)
+    detail = models.TextField(blank=True,)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.rating} - {self.comment}'
