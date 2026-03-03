6b0f2edece2344885f8ffa65ca6402cf6b9fdaa7
helmut <helmut.andrey@gmail.com>
2026-03-03 15:13:04 +0500

    Fix gitignore move base.html add product-list.html

--- products/urls.py
+++ products/urls.py
@@ -4,12 +4,13 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 
 
-from products.views import ProductDetailView
+from products.views import ProductDetailView, ProductListView
 from users import views
 
 app_name = 'products'
 
 urlpatterns = [
     path('<slug:slug>/', ProductDetailView.as_view(), name='product'),
+    path('', ProductListView.as_view(), name='products_list')
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from products.models import Product, Review
+from django.views.generic import DetailView, TemplateView, ListView
+from products.models import Product, Review, Category
 
 
 # Create your views here.
@@ -16,4 +16,15 @@ class ProductDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['reviews'] = Review.objects.filter(product=self.object)
+        return context
+
+class ProductListView(ListView):
+    template_name = 'products/product-list.html'
+    queryset = Product.objects.filter(is_active=True)
+    context_object_name = 'products'
+
+
+    def get_context_data(self, *, object_list = ..., **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
         return context
