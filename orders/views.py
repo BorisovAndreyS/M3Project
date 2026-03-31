67f311fb8760a48cc64f0420d017b473380f9f00
helmut <helmut.andrey@gmail.com>
2026-03-31 08:00:47 +0500

    fix delete comment

--- orders/views.py
+++ orders/views.py
@@ -121,9 +121,7 @@ class CartDetailView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # cart = get_or_create_cart(self.request)
         context['cart'] = get_or_create_cart(self.request)
-
         return context
 
 
